b1348add667fdb0661d63045f7c23e66185189e5
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-02 09:22:32 -0500

    Add mouse keys to layer 4, LT4 toggle to right-side BKSP key

--- kyria/keymaps/hands-down/rules.mk
+++ kyria/keymaps/hands-down/rules.mk
@@ -3,7 +3,7 @@ OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = no        # Enable the Leader Key feature
-MOUSEKEY_ENABLE = no	# Enable the Mousekeys feature
+MOUSEKEY_ENABLE = yes	# Enable the Mousekeys feature
 COMBO_ENABLE = no		# Enable the Combos feature. IE, f+c = CTL + V for paste, etc. 
 TAP_DANCE_ENABLE = yes # Enable the Tap Dance feature. Single tap = keycode, double-tap = difference keycode, etc.
 

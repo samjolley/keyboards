33dcd30f2847b03ed1e933a7f4f7129bf69624cd
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-03 15:09:13 -0500

    Update keymap.c
    Corrected number keycodes. Mistakenly had NUMPAD number keys that required NUMLOCK to function. Replaced with standard KC_# style keys.

--- kyria/keymaps/hands-down/keymap.c
+++ kyria/keymaps/hands-down/keymap.c
@@ -144,9 +144,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
 [ADJUST] = LAYOUT
-    (KC_GRV , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                                                           KC_PSLS , KC_P7   , KC_P8    , KC_P9 , KC_PMNS , KC_TRNS, 
-    KC_TILD , KC_HASH , KC_DLR  , KC_LPRN , KC_RPRN , KC_TRNS ,                                                           KC_PAST , KC_P4   , KC_P5    , KC_P6 , KC_PPLS , KC_TRNS, 
-    KC_PIPE , KC_TRNS , KC_TRNS , KC_LT   , KC_GT   , KC_TILD , KC_TRNS , KC_TRNS ,                   KC_TRNS , KC_TRNS , KC_P0   , KC_P1   , KC_P2    , KC_P3 , KC_PEQL , KC_TRNS, 
+    (KC_GRV , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                                                           KC_PSLS , KC_7   , KC_8    , KC_9 , KC_MINS , KC_TRNS, 
+    KC_TILD , KC_HASH , KC_DLR  , KC_LPRN , KC_RPRN , KC_TRNS ,                                                           KC_PAST , KC_4   , KC_5    , KC_6 , KC_PLUS , KC_TRNS, 
+    KC_PIPE , KC_TRNS , KC_TRNS , KC_LT   , KC_GT   , KC_TILD , KC_TRNS , KC_TRNS ,                   KC_TRNS , KC_TRNS , KC_0    , KC_1   , KC_2    , KC_3 , KC_EQL  , KC_TRNS, 
                                   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,                   KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS),
 };
 

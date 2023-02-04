014c02502801cb464c879fed130b9c43328e2d82
Sam Jolley <samjolley@users.noreply.github.com>
2023-02-04 09:48:14 -0500

    Update keymap.c

--- kyria/keymaps/hands-down/keymap.c
+++ kyria/keymaps/hands-down/keymap.c
@@ -1,4 +1,4 @@
-$$$$$$$$$/* Copyright 2023 Sam Jolley
+/* Copyright 2023 Sam Jolley
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
